f1b692ff239e00bf379952c2d90d33dc945708c1
elteron1 <90413323+elteron1@users.noreply.github.com>
2022-01-22 16:44:05 +0100

    Add files via upload

---
+++ fast-arch-setup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+echo "Just quick setup script for my arch setup"
+sudo pacman -Syu
+sudo pacman -S --needed base-devel git wget yajl
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+sudo yay -S pamac-aur
+sudo nano /etc/pacman.conf
+sudo pacman -Syyy
+sudo pacman -S lib32-nvidia-utils
+sudo pacman -S ttf-liberation wqy-zenhei steam unzip bluez bluez-utils pulseaudio-bluetooth pulseaudio discord winetricks btop htop neofetch firefox gimp gparted openssh obs-studio ulauncher vi vim virtualbox virtualbox-guest-iso
+sudo systemctl enable bluetooth.service
+pamac build davinci-resolve
+sudo modprobe vboxdrv
+sudo usermod -aG vboxusers matej
+
