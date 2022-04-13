ec0fd8b50e87a0695f2a77414950d427fdbd11e3
Elteron <90413323+elteron1@users.noreply.github.com>
2022-04-13 17:22:21 +0200

    Added npm package

--- fast-arch-setup.sh
+++ fast-arch-setup.sh
@@ -11,7 +11,7 @@ sudo nano /etc/pacman.conf
 sudo pacman -Syyy
 sudo pacman -S lib32-nvidia-utils
 sudo pacman -S git ttf-liberation wqy-zenhei steam unzip bluez bluez-utils pulseaudio-bluetooth pulseaudio discord winetricks btop htop neofetch gparted openssh obs-studio vi vim virtualbox virtualbox-guest-iso noto-fonts-cjk noto-fonts-emoji 
-sudo yay -S brave-bin anydesk-bin etcher-bin clion-cmake clion-jre clion-gdb clion-lldb clion webstorm-jre webstorm davinci-resolve
+sudo yay -S brave-bin anydesk-bin etcher-bin clion-cmake clion-jre clion-gdb clion-lldb clion webstorm-jre webstorm davinci-resolve npm
 sudo systemctl enable bluetooth.service
 sudo modprobe vboxdrv
 sudo usermod -aG vboxusers matej
